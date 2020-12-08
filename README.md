949255ab56f51c66f34753a86a5542843fb690ad
Matias Garcia <mgarcia@talisbio.com>
2020-12-07 23:38:11 -0800

    Initial commit of rolling-sum workflow and CDC data pulled Dec 6, 2020

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# covid_analysis
+# Independent analysis of COVID-19 data
+
+Starting with rolling sums of weekly death counts to see if the data matches what people say about it.
+
+## Dataset
+
+Weekly death counts by jurisdiction and cause group pulled from the CDC website at https://data.cdc.gov/NCHS/Weekly-counts-of-death-by-jurisdiction-and-cause-o/u6jv-9ijr on Dec. 6, 2020
+
+## Approach
+
+Keeping it simple to start
+1. Group by jurisdiction and week end date only, just sum all cause groups for now
+2. Separate states/jurisdictions from the country as a whole
+3. Calculate rolling sums of 1, 2, 4, 13, 26, and 52 weeks for some insight into the gravity of the current situation.
+
+TODO:
+1. Add trend line
+2. Adjust for population growth
+3. Add data prior to 2015
+4. Start looking into cause groups
+5. Try and find ICU capacity data and compare with flu 2018
+
---
+++ main.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plotRollingSum(df, weeks, title, legend=True, figsize=[24,24]):
+    title += f' - {weeks} Week Rolling Sum'
+    if weeks > 1:
+        rolling = df.rolling(weeks).sum()
+    else:
+        rolling = df
+    axes = rolling.plot(figsize=figsize, legend = legend)
+    plt.title(title)
+
+    if not legend:
+        # Annotate jurisdiction names instead of using a legend
+        for label, content in rolling.items():
+            offset = content.idxmax()
+            plt.annotate(label,
+                         (offset, content[offset]),
+                         textcoords="offset points",
+                         xytext=(0,0),
+                         ha='center')
+
+    plt.savefig(title + ".png", dpi=600)
+    plt.show()
+
+df = pd.read_csv('Weekly_counts_of_death_by_jurisdiction_and_cause_of_death_2020-12-06.csv',
+                 # index_col=['Jurisdiction',
+                 #            'Cause Group',
+                 #            'Week Ending Date'],
+                 usecols=['Week Ending Date',
+                          'Jurisdiction',
+                          'Cause Group',
+                          'Number of Deaths',
+                          'Type'],
+                 dtype={'Cause Group' : 'category',
+                        'Jurisdiction' : 'category',
+                        'Type': 'category'},
+                 parse_dates=['Week Ending Date'],
+                 cache_dates=True)
+
+# Remove duplicates created with the 'Type' column *sigh*
+df = df[df['Type'] == 'Unweighted']
+del df['Type']
+
+jd = df.groupby(['Week Ending Date', 'Jurisdiction']).sum('Number of Deaths').unstack()
+states = jd.drop(columns=jd.filter(like='United States').columns)
+usa = jd.filter(like='United States')
+
+for weeks in [52, 26, 13, 4, 2, 1]:
+    plotRollingSum(states, weeks, "Total Deaths by CDC Jurisdiction", False)
+    plotRollingSum(usa, weeks, "Total Deaths USA", True)
