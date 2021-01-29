575606695be7f4bc6aa07f5c9585c577f9c255ee
Matias Garcia <mgarcia@talisbio.com>
2021-01-29 00:49:29 -0800

    Add stacked area graph, normalized subplots, normalized line graph. Reorder stacked graph for states (North Carolina) that don't seem to keep up reporting so it's not so confusing.

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-def plotRollingSum(df, weeks, title, legend=True, figsize=[24,24], dpi=300, prefix='./plots'):
+def plotRollingSum(df, weeks, title, prefix='./plots', dpi=300, normalize=False, annotate=False, **kwargs):
     title += f' - {weeks} Week Rolling Sum'
     if weeks > 1:
         rolling = df.rolling(weeks).sum()
     else:
         rolling = df
-    axes = rolling.plot(figsize=figsize, legend = legend)
-    plt.title(title)
 
-    if not legend:
-        # Annotate jurisdiction names instead of using a legend
+    # Cut off unused part of sum
+    rolling = rolling[weeks:]
+
+    # Optionally normalize all the curves to the mean
+    if normalize:
+        rolling /= rolling.mean()
+
+    axes = rolling.plot(title=title, **kwargs)
+
+    # Annotate jurisdiction names
+    if annotate:
         for label, content in rolling.items():
             offset = content.idxmax()
             plt.annotate(label,
@@ -23,10 +29,11 @@ def plotRollingSum(df, weeks, title, legend=True, figsize=[24,24], dpi=300, pref
                          ha='center')
 
     plt.savefig(f'{prefix}/{title}-{dpi}dpi.png', dpi=dpi)
-    plt.show()
+    plt.close()
 
 # Ensure plots folder exists
-Path("./plots").mkdir(parents=True, exist_ok=True)
+dataPath = Path("./plots")
+dataPath.mkdir(parents=True, exist_ok=True)
 
 df = pd.read_csv('Weekly_counts_of_death_by_jurisdiction_and_cause_of_death.csv',
                  # index_col=['Jurisdiction',
@@ -44,13 +51,41 @@ df = pd.read_csv('Weekly_counts_of_death_by_jurisdiction_and_cause_of_death.csv'
                  cache_dates=True)
 
 # Remove duplicates created with the 'Type' column *sigh*
-df = df[df['Type'] == 'Unweighted']
-del df['Type']
+unweighted = df[df['Type'] == 'Unweighted']
+del unweighted['Type']
 
-jd = df.groupby(['Week Ending Date', 'Jurisdiction']).sum('Number of Deaths').unstack()
+jd = unweighted.groupby(['Week Ending Date', 'Jurisdiction']).sum('Number of Deaths').unstack()
+
+# Reorder columns from most deaths to least
+jd = jd.reindex(columns=jd.sum().sort_values(ascending=False).index)
+
 states = jd.drop(columns=jd.filter(like='United States').columns)
 usa = jd.filter(like='United States')
 
-for weeks in [52, 26, 13, 4, 2, 1]:
-    plotRollingSum(states, weeks, "Total Deaths by CDC Jurisdiction", False)
-    plotRollingSum(usa, weeks, "Total Deaths USA", True)
+
+# Stacked plots requires reordering problematic states that don't keep up reporting or it looks confusing
+problematicStates = ['North Carolina']
+statesColumns = list(states.columns)
+for item in problematicStates:
+    statesColumns.sort(key=lambda s: s[1].startswith(item))
+
+stackedStates = states.reindex(columns=statesColumns)
+
+for weeks in [52, 26, 13, 4, 2]:
+    # All jurisdictions in subplots
+    plotRollingSum(jd, weeks, title="Total Deaths by CDC Jurisdiction Normalized to Mean - Subplots", normalize=True,
+                   figsize=[36, 24], subplots=True, layout=[18, 3], sharey=True, grid=True)
+
+    # Stacked in an area graph
+    plotRollingSum(stackedStates, weeks, "Total Deaths by CDC Jurisdiction - Stacked", normalize=False,
+                   figsize=[24,24], kind='area', legend='reverse', grid=True)
+
+    plotRollingSum(states, weeks, "Total Deaths by CDC Jurisdiction - Line", normalize=False, annotate=True,
+                   figsize=[24, 24], legend=True, grid=True)
+    plotRollingSum(states, weeks, "Normalized Deaths by CDC Jurisdiction - Line", normalize=True, annotate=True,
+                   figsize=[24, 24], legend='reverse', grid=True)
+
+    plotRollingSum(usa, weeks, "Total Deaths USA", annotate=True, figsize=[24,24])
+
+    #for jurisdiction in jd.columns:
+    #    plotRollingSum(jd.filter(like=jurisdiction[1]), weeks=52, title="Total Deaths ")
